8220a5a9aefcfcaf15ad3a6c7a8203fa9448650d
Begonia. <106400715+Begoniarun@users.noreply.github.com>
2024-12-11 22:47:56 +0800

    Update main.rs

--- env_looger/src/main.rs
+++ env_looger/src/main.rs
@@ -5,11 +5,15 @@ use std::io::Write;
 
 fn main() {
     //Initialize env_logger
+    //buf：实现Write trait 的对象，通常代表日志信息要写入的目标缓冲区
+    // 调用 writeln! 宏向这个缓冲区写入格式化后的日志内容。
     Builder::new().format(|buf, record| {
+        //
         writeln!(buf,
                  "[{}] - {} - {}",
                  chrono::Local::now().format("%Y-%m-%d %H:%M:%S"), // Timestamp
-                 record.level(),                                  // level of log
+                 record.level(),
+                 //获取实际要记录的消息内容，作为日志的具体信息部分。
                  record.args()
         )
     })
